c38955f39101ce4200610b5fc6f8ed3bda0820f2
Toan Nguyen <toan-huu.nguyen@kolekto.nl>
2018-10-18 15:12:52 +0700

    Human interaction and Monitor patterns

---
+++ precompiled/DurablePatterns/FunctionHumanInteracion.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VSSample.DurablePatterns
+{
+    public class ApprovalRequest
+    {
+        public bool IsApproved { get; set; }
+    }
+
+    public static class FunctionHumanInteracion
+    {
+        [FunctionName("Human_RequestApproval")]
+        public static async Task<string> Run([OrchestrationTrigger] DurableOrchestrationContext ctx)
+        {
+            string approvalObj = "Approval remove durable function";
+            await ctx.CallActivityAsync("RequestApproval", approvalObj);
+            using (var timeoutCts = new CancellationTokenSource())
+            {
+                DateTime dueTime = ctx.CurrentUtcDateTime.AddMinutes(2);
+                Task durableTimeout = ctx.CreateTimer(dueTime, timeoutCts.Token);
+
+                Task<ApprovalRequest> approvalEvent = ctx.WaitForExternalEvent<ApprovalRequest>("ApprovalEvent");
+                if (approvalEvent == await Task.WhenAny(approvalEvent, durableTimeout))
+                {
+                    timeoutCts.Cancel();
+                    return await ctx.CallActivityAsync<string>("ProcessApproval", approvalEvent.Result);
+                }
+                else
+                {
+                    return await ctx.CallActivityAsync<string>("Escalate", "thanh.tran@devinition.com.vn");
+                }
+            }
+        }
+
+        [FunctionName("RequestApproval")]
+        public static string RequestApproval([ActivityTrigger] string name)
+        {
+            return $"Request approval {name} is created";
+        }
+
+        [FunctionName("ProcessApproval")]
+        public static string ProcessApproval([ActivityTrigger] ApprovalRequest approvalRequest)
+        {
+            if (approvalRequest.IsApproved)
+            {
+                return $"Request is approved";
+            }
+            else
+            {
+                return $"Request is rejected";
+            }
+        }
+
+        [FunctionName("Escalate")]
+        public static string Escalate([ActivityTrigger] string name)
+        {
+            return $"I want to escalate {name} because he didn't approve my request";
+        }
+
+        [FunctionName("RequestApprove")]
+        public static async Task RequestApprove([HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = "approval/{instanceId}")] HttpRequestMessage req,
+            [OrchestrationClient] DurableOrchestrationClient client,
+            string instanceId)
+        {
+            dynamic eventData = await req.Content.ReadAsAsync<object>();
+            await client.RaiseEventAsync(instanceId, "ApprovalEvent", eventData);
+        }
+    }
+}
---
+++ precompiled/DurablePatterns/FunctionMonitoring.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VSSample.DurablePatterns
+{
+
+    public class SendAlertParameter
+    {
+        public string JobStatusUrl { get; set; }
+    }
+    public class FunctionMonitoring
+    {
+        private static HttpClient client = new HttpClient();
+
+        [FunctionName("Monitor_SendAlert")]
+        public static async Task Run(
+            [OrchestrationTrigger] DurableOrchestrationContext ctx)
+        {
+            SendAlertParameter param = ctx.GetInput<SendAlertParameter>();
+            DateTime expiryTime = DateTime.Now.AddMinutes(3);
+
+            while (ctx.CurrentUtcDateTime < expiryTime)
+            {
+                var jobStatus = await ctx.CallActivityAsync<string>("GetJobStatus", param.JobStatusUrl);
+                if (jobStatus == "Completed")
+                {
+                    // Perform action when condition met
+                    await ctx.CallActivityAsync("SendAlert", "Test send alert message");
+                    break;
+                }
+
+                // Orchestration will sleep until this time
+                var nextCheck = ctx.CurrentUtcDateTime.AddSeconds(5);
+                await ctx.CreateTimer(nextCheck, CancellationToken.None);
+            }
+        }
+
+        [FunctionName("SendAlert")]
+        public static async Task<string> SendAlert([ActivityTrigger] string message)
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("AccessKey", "RV9aNyXY31IIAAsCmosAgE9zl");
+            var options = new
+            {
+                Recipients = "+84915293595",
+                Body = message,
+                Originator = "Toan"
+            };
+
+            await client.PostAsJsonAsync("https://rest.messagebird.com/messages", options);
+
+            return JsonConvert.SerializeObject(options);
+        }
+
+        [FunctionName("GetJobStatus")]
+        public static async Task<string> GetJobStatus([ActivityTrigger] string url)
+        {
+            var jobStatus = await client.GetAsync(url);
+            string result = await jobStatus.Content.ReadAsStringAsync();
+            if (!string.IsNullOrEmpty(result))
+            {
+                var statusTest = await jobStatus.Content.ReadAsAsync<DurableOrchestrationStatus>(new[] { new JsonMediaTypeFormatter() });
+                JObject status = JsonConvert.DeserializeObject<JObject>(result);
+                if (status != null)
+                {
+                    return (string)status["runtimeStatus"];
+                }
+            }
+
+            return null;
+        }
+    }
+}
